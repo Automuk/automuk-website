4ca29e6bbe62b617d473db712f3c0e3b0aee143b
draunzler <shadymeister2017@gmail.com>
2026-02-07 12:58:03 +0000

    changed next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
+  assetPrefix: process.env.NODE_ENV === "production" ? "" : undefined,
 };
 
 export default nextConfig;
